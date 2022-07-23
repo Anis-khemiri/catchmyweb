2e2d5b46e5b0fd5d8a51134a1c9a93dcdd8273c6
Anis-khemiri <aniskhemiri99@gmail.com>
2022-07-23 17:43:14 +0100

    update 2

--- components/Works.js
+++ components/Works.js
@@ -1,13 +1,12 @@
-
 import Image from 'next/image';
+import dataPortfolio from './data/dataPortfolio';
 
 export default function Works() {
   return (
     <div>
-     <div className="our-work text-center pt-5 pb-5">
+      <div className="our-work text-center pt-5 pb-5">
         <div className="container">
           <div className="main-title mt-5 mb-5 position-relative">
-          
             <h2>We Make This</h2>
             <p className="text-black-50 text-uppercase">Prepare To Be Amazed</p>
           </div>
@@ -19,52 +18,30 @@ export default function Works() {
             <li>App</li>
           </ul>
           <div className="row">
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-                <Image src="/project1.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project2.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project3.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project4.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project5.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project6.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project1.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
-            <div className="col-sm-6 col-md-4 col-lg-3">
-              <div className="box mb-3 bg-white" data-work="Application">
-              <Image src="/project2.png" width="296px" height="220px" alt="" />
-              </div>
-            </div>
+            {dataPortfolio.projects.map((project) => {
+              return (
+                <div className="col-sm-6 col-md-4 col-lg-3" key={project.id}>
+                  <div className="box mb-3 bg-white" data-work="Application">
+                    <Image
+                      src={project.image}
+                      width="296px"
+                      height="220px"
+                      alt=""
+                    />
+                    <h4>{project.name}</h4>
+                  </div>
+                </div>
+              );
+            })}
           </div>
+
           <div className="d-flex justify-content-center mt-5">
-            <a className="btn rounded-pill main-btn text-uppercase" href="#">I Want More</a>
+            <a className="btn rounded-pill main-btn text-uppercase" href="#">
+              I Want More
+            </a>
           </div>
         </div>
-      </div>  
+      </div>
     </div>
-  )
+  );
 }
---
+++ components/data/dataPortfolio.js
@@ -0,0 +1,104 @@
+ const dataPortfolio = {
+    projects : [
+    
+   
+          {
+                
+                name: "Nike Slim Shirt",
+                category: 'Shirts',
+            image: '/project1.png',
+                price: 320,
+                countInStock: 50,
+                brand: 'Nike',
+                rating: 2.5,
+                numReviews: 10,
+                description: 'high quality product',
+            },
+            {
+               
+                name: 'Adidas Fit Shirt',
+                category: 'Shirts',
+                image: '/project2.png',
+                price: 120,
+                countInStock: 20,
+                brand: 'Adidas',
+                rating: 4.5,
+                numReviews: 10,
+                description: 'high quality product',
+            },
+            {
+               
+                name: 'Lacost Free Shirt',
+                category: 'Shirts',
+                image: '/project3.png',
+                price: 220,
+                countInStock: 0,
+                brand: 'Lacoste',
+                rating: 4.8,
+                numReviews: 17,
+                description: 'high quality product',
+            },
+            {
+                
+                name: 'Niko Slim Shirt',
+                category: 'Pants',
+                image: '/project4.png',
+                price: 78,
+                countInStock: 10,
+                brand: 'Nike',
+                rating: 4.5,
+                numReviews: 14,
+                description: 'high quality product',
+            },
+            {
+               
+                name: 'Puma Slim Pant',
+                category: 'Pants',
+                image: '/project5.png',
+                price: 65,
+                countInStock: 45,
+                brand: 'Puma',
+                rating: 4.5,
+                numReviews: 10,
+                description: 'high quality product',
+            },
+            {
+              
+                name: 'Adidas Fit Pants',
+                category: 'Pants',
+                image: '/project6.png',
+                price: 80,
+                countInStock: 10,
+                brand: 'Adidas',
+                rating: 4.5,
+                numReviews: 10,
+                description: 'high quality product',
+            },
+             {   name: 'Adidas Fit Pants',
+                category: 'Pants',
+                image: '/project6.png',
+                price: 80,
+                countInStock: 10,
+                brand: 'Adidas',
+                rating: 4.5,
+                numReviews: 10,
+                description: 'high quality product',
+            },
+            {
+                name: 'Adidas Fit Pants',
+                category: 'Pants',
+                image: '/project6.png',
+                price: 80,
+                countInStock: 10,
+                brand: 'Adidas',
+                rating: 4.5,
+                numReviews: 10,
+                description: 'high quality product',
+            }
+            
+        ],
+    
+};
+
+   
+export default dataPortfolio;
